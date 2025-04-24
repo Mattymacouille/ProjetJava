364b5b6122ec57e4c7315cbb981977fc2d124ad5
marcoofrt <marco.frt99@gmail.com>
2025-04-24 19:13:00 +0200

    ajout des getters et setters de Client dans Admin

--- src/Modele/Admin.java
+++ src/Modele/Admin.java
@@ -6,4 +6,14 @@ public class Admin extends Utilisateur{
     public Admin(String userMdp, String userMail) {
         super(userMdp, userMail);
     }
+
+    public String getNom(Client client) {return client.getNom();}
+    public String getPrenom(Client client) {return client.getPrenom();}
+    public LocalDate getDateNaissance(Client client) {return client.getDateNaissance();}
+    public int getClientId(Client client) {return client.getClientId();}
+
+    public void setNom(Client client, String nom) {client.setNom(nom);}
+    public void setPrenom(Client client, String prenom) {client.setPrenom(prenom);}
+    public void setDateNaissance(Client client, LocalDate dateNaissance) {client.setDateNaissance(dateNaissance);}
+    public void setClientId(Client client, int clientId) {client.setClientId(clientId);}
 }
