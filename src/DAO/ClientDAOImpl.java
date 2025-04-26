5e36cf671a20a353af09a3d24d1848852b075b9e
Mattymacouille <matteo.devaux01@gmail.com>
2025-04-26 15:25:41 +0200

    DAO CLIENT

---
+++ src/DAO/ClientDAOImpl.java
@@ -0,0 +1,159 @@
+package DAO;
+
+// import des packages
+import Modele.Client;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ * implémentation MySQL du stockage dans la base de données des méthodes définies dans l'interface
+ * ClientDAO.
+ */
+public class ClientDAOImpl implements ClientDAO {
+    private DaoFactory daoFactory;
+
+    public ClientDAOImpl(DaoFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+
+    @Override
+    public ArrayList<Client> getAll() {
+        ArrayList<Client> listeClients = new ArrayList<>();
+
+        try {
+            Connection connexion = daoFactory.getConnection();
+            Statement statement = connexion.createStatement();
+            ResultSet resultats = statement.executeQuery("SELECT * FROM clients");
+
+            while (resultats.next()) {
+                int clientId = resultats.getInt("clientID");
+                String userMail = resultats.getString("userMail");
+                String userMdp = resultats.getString("userMdp");
+                String nom = resultats.getString("clientNom");
+                String prenom = resultats.getString("clientPrenom");
+                LocalDate dateNaissance = resultats.getDate("clientDateNaissance").toLocalDate();
+
+                Client client = new Client(userMdp, userMail, nom, prenom, dateNaissance, clientId);
+                listeClients.add(client);
+            }
+
+            resultats.close();
+            statement.close();
+            connexion.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Création de la liste de clients impossible");
+        }
+
+        return listeClients;
+    }
+
+    public void ajouter(Client client) {
+        try {
+            Connection connexion = daoFactory.getConnection();
+
+            String sql = "INSERT INTO clients (userMail, userMdp, clientNom, clientPrenom, clientDateNaissance) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = connexion.prepareStatement(sql);
+
+            statement.setString(1, client.getUserMail());
+            statement.setString(2, client.getUserMdp());
+            statement.setString(3, client.getNom());
+            statement.setString(4, client.getPrenom());
+            statement.setDate(5, Date.valueOf(client.getDateNaissance()));
+
+            statement.executeUpdate();
+
+            statement.close();
+            connexion.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Ajout du client impossible");
+        }
+    }
+
+    public Client chercher(int id) {
+        Client client = null;
+
+        try {
+            Connection connexion = daoFactory.getConnection();
+            String sql = "SELECT * FROM clients WHERE clientID = ?";
+            PreparedStatement statement = connexion.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet resultats = statement.executeQuery();
+
+            if (resultats.next()) {
+                int clientId = resultats.getInt("clientID");
+                String userMail = resultats.getString("userMail");
+                String userMdp = resultats.getString("userMdp");
+                String nom = resultats.getString("clientNom");
+                String prenom = resultats.getString("clientPrenom");
+                LocalDate dateNaissance = resultats.getDate("clientDateNaissance").toLocalDate();
+
+                client = new Client(userMdp, userMail, nom, prenom, dateNaissance, clientId);
+            }
+
+            resultats.close();
+            statement.close();
+            connexion.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Client non trouvé dans la base de données");
+        }
+
+        return client;
+    }
+
+    public Client modifier(Client client) {
+        try {
+            Connection connexion = daoFactory.getConnection();
+
+            String sql = "UPDATE clients SET userMail = ?, userMdp = ?, clientNom = ?, clientPrenom = ?, clientDateNaissance = ? WHERE clientID = ?";
+            PreparedStatement statement = connexion.prepareStatement(sql);
+
+            statement.setString(1, client.getUserMail());
+            statement.setString(2, client.getUserMdp());
+            statement.setString(3, client.getNom());
+            statement.setString(4, client.getPrenom());
+            statement.setDate(5, Date.valueOf(client.getDateNaissance()));
+            statement.setInt(6, client.getClientId());
+
+            statement.executeUpdate();
+
+            statement.close();
+            connexion.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Modification du client impossible");
+        }
+
+        return client;
+    }
+
+    @Override
+    public void supprimer(Client client) {
+        try {
+            Connection connexion = daoFactory.getConnection();
+            connexion.setAutoCommit(false);
+
+            String sqlCommandes = "DELETE FROM commander WHERE clientID = ?";
+            PreparedStatement statementCommandes = connexion.prepareStatement(sqlCommandes);
+            statementCommandes.setInt(1, client.getClientId());
+            statementCommandes.executeUpdate();
+
+            String sqlClient = "DELETE FROM clients WHERE clientID = ?";
+            PreparedStatement statementClient = connexion.prepareStatement(sqlClient);
+            statementClient.setInt(1, client.getClientId());
+            statementClient.executeUpdate();
+
+            connexion.commit();
+
+            statementCommandes.close();
+            statementClient.close();
+            connexion.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Suppression du client impossible");
+        }
+    }
+}
---
+++ src/Modele/Client.java
@@ -0,0 +1,28 @@
+package Modele;
+
+import java.time.LocalDate;
+
+public class Client extends Utilisateur{
+    private String nom;
+    private String prenom;
+    private LocalDate dateNaissance;
+    private int clientId;
+
+    public Client(String userMdp, String userMail, String nom, String prenom, LocalDate dateNaissance, int clientId) {
+        super(userMdp, userMail);
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.clientId = clientId;
+    }
+
+    public String getNom() {return nom;}
+    public String getPrenom() {return prenom;}
+    public LocalDate getDateNaissance() {return dateNaissance;}
+    public int getClientId() {return clientId;}
+
+    public void setNom(String nom) {this.nom = nom;}
+    public void setPrenom(String prenom) {this.prenom = prenom;}
+    public void setDateNaissance(LocalDate dateNaissance) {this.dateNaissance = dateNaissance;}
+    public void setClientId(int clientId) {this.clientId = clientId;}
+}
